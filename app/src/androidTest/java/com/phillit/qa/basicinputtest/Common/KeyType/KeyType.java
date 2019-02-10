e5a2a3f0c05fa8fe0d425450383aad14f910d4ba
jeongbeen.son <wjdqls62@naver.com>
2019-02-10 19:15:59 +0900

    * 가로모드 및 세로모드 FLAG변수명 변경
    * 천지인 FLAG변수명 추가

--- app/src/androidTest/java/com/phillit/qa/basicinputtest/Common/KeyType/KeyType.java
+++ app/src/androidTest/java/com/phillit/qa/basicinputtest/Common/KeyType/KeyType.java
@@ -2,11 +2,12 @@ package com.phillit.qa.basicinputtest.Common.KeyType;
 
 public abstract class KeyType {
     // Screen Orientation & KeyType
-    public static final int QWERTY_PORTRAIT = 0;
-    public static final int QWERTY_LANDSCAPE = 1;
+    public static final int PORTRAIT = 0;
+    public static final int LANDSCAPE = 1;
 
     public static final int QWERTY_KOREA = 100;
     public static final int QWERTY_ENGLISH = 101;
+    public static final int CHUNJIIN = 102;
 
     abstract public void input(String args);
     abstract public void input(StringBuffer args);
