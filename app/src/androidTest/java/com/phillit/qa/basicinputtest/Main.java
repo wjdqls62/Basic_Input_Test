0c0ba3d6ede2c39f9157fb0f7fba7bec5c188bdf
jeongbeen.son <wjdqls62@naver.com>
2019-03-09 18:11:17 +0900

    Revert "* recommendation word Branch init"
    This reverts commit 71a3094

--- app/src/androidTest/java/com/phillit/qa/basicinputtest/Main.java
+++ app/src/androidTest/java/com/phillit/qa/basicinputtest/Main.java
@@ -9,6 +9,18 @@ import android.util.Log;
 import com.phillit.qa.basicinputtest.Common.Device;
 import com.phillit.qa.basicinputtest.Common.KeyType.KeyType;
 import com.phillit.qa.basicinputtest.Common.TestCaseParser;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_01_KOR_QEWRTY_PORT;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_02_KOR_QWERTY_LAND;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_03_ENG_QWERTY_PORT;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_04_ENG_QWERTY_LAND;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_05_KOR_CHUNJIIN_PORT;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_06_KOR_CHUNJIIN_LAND;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_07_KOR_SKY_PORT;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_08_KOR_SKY_LAND;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_09_KOR_NARAGUL_PORT;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_10_KOR_NARAGUL_LAND;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_11_KOR_DANMOUM_PORT;
+import com.phillit.qa.basicinputtest.TestCase.TestCase_12_KOR_DANMOUM_LAND;
 
 import org.junit.After;
 import org.junit.Before;
@@ -36,6 +48,197 @@ public class Main {
     @Test
     public void Test(){
 
+        try{
+            Log.i("@@@", "KOR_QWERTY_PORTRAIT - " + testPlan.getTestPlan(KeyType.KOR_QWERTY, KeyType.PORTRAIT));
+            // 한글QWERTY(세로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_QWERTY, KeyType.PORTRAIT)){
+                TestCase_01_KOR_QEWRTY_PORT TC01 = new TestCase_01_KOR_QEWRTY_PORT(device, "KOR_QWERTY_PORTRAIT");
+                runTime += "================= KOR_QWERTY_PORTRAIT =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC01.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "KOR_QWERTY_LANDSCAPE - " + testPlan.getTestPlan(KeyType.KOR_QWERTY, KeyType.LANDSCAPE));
+            // 한글QWERTY(가로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_QWERTY, KeyType.LANDSCAPE)){
+                TestCase_02_KOR_QWERTY_LAND TC02 = new TestCase_02_KOR_QWERTY_LAND(device, "KOR_QWERTY_LANDSCAPE");
+                runTime += "================= KOR_QWERTY_LANDSCAPE =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC02.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "ENG_QWERTY_PORTRAIT - " + testPlan.getTestPlan(KeyType.ENG_QWERTY, KeyType.PORTRAIT));
+            // 영문QWERTY(세로모드) 입력
+            if(testPlan.getTestPlan(KeyType.ENG_QWERTY, KeyType.PORTRAIT)){
+                TestCase_03_ENG_QWERTY_PORT TC03 = new TestCase_03_ENG_QWERTY_PORT(device, "ENG_QWERTY_PORTRAIT");
+                runTime += "================= ENG_QWERTY_PORTRAIT =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC03.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "ENG_QWERTY_LANDSCAPE - " + testPlan.getTestPlan(KeyType.ENG_QWERTY, KeyType.LANDSCAPE));
+            // 영문QWERTY(가로모드) 입력
+            if(testPlan.getTestPlan(KeyType.ENG_QWERTY, KeyType.LANDSCAPE)){
+                TestCase_04_ENG_QWERTY_LAND TC04 = new TestCase_04_ENG_QWERTY_LAND(device, "ENG_QWERTY_LANDSCAPE");
+                runTime += "================= ENG_QWERTY_LANDSCAPE =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC04.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "CHUNJIIN_PORTRAIT - " + testPlan.getTestPlan(KeyType.KOR_CHUNJIIN, KeyType.PORTRAIT));
+            // 천지인(세로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_CHUNJIIN, KeyType.PORTRAIT)){
+                TestCase_05_KOR_CHUNJIIN_PORT TC05 = new TestCase_05_KOR_CHUNJIIN_PORT(device, "CHUNJIIN_PORTRAIT");
+                runTime += "================= CHUNJIIN_PORTRAIT =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC05.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "CHUNJIIN_LANDSCAPE - " + testPlan.getTestPlan(KeyType.KOR_CHUNJIIN, KeyType.LANDSCAPE));
+            // 천지인(가로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_CHUNJIIN, KeyType.LANDSCAPE)){
+                TestCase_06_KOR_CHUNJIIN_LAND TC06 = new TestCase_06_KOR_CHUNJIIN_LAND(device, "CHUNJIIN_LANDSCAPE");
+                runTime += "================= CHUNJIIN_LANDSCAPE =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC06.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "SKY_PORTRAIT - " + testPlan.getTestPlan(KeyType.KOR_SKY, KeyType.PORTRAIT));
+            // KOR_SKY(세로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_SKY, KeyType.PORTRAIT)){
+                TestCase_07_KOR_SKY_PORT TC07 = new TestCase_07_KOR_SKY_PORT(device, "SKY_PORTRAIT");
+                runTime += "================= SKY_PORTRAIT =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC07.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "SKY_LANDSCAPE - " + testPlan.getTestPlan(KeyType.KOR_SKY, KeyType.LANDSCAPE));
+            // KOR_SKY(가로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_SKY, KeyType.LANDSCAPE)){
+                TestCase_08_KOR_SKY_LAND TC08 = new TestCase_08_KOR_SKY_LAND(device, "SKY_LANDSCAPE");
+                runTime += "================= SKY_LANDSCAPE =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC08.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "NARAGUL_PORTRAIT - " + testPlan.getTestPlan(KeyType.KOR_NARAGUL, KeyType.PORTRAIT));
+            // 나랏글(세로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_NARAGUL, KeyType.PORTRAIT)){
+                TestCase_09_KOR_NARAGUL_PORT TC09 = new TestCase_09_KOR_NARAGUL_PORT(device, "NARAGUL_PORTRAIT");
+                runTime += "================= NARAGUL_PORTRAIT =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC09.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "NARAGUL_LANDSCAPE - " + testPlan.getTestPlan(KeyType.KOR_NARAGUL, KeyType.LANDSCAPE));
+            // 나랏글(가로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_NARAGUL, KeyType.LANDSCAPE)){
+                TestCase_10_KOR_NARAGUL_LAND TC10 = new TestCase_10_KOR_NARAGUL_LAND(device, "NARAGUL_LANDSCAPE");
+                runTime += "================= NARAGUL_LANDSCAPE =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC10.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "DANMOUM_PORTRAIT - " + testPlan.getTestPlan(KeyType.KOR_DANMOUM, KeyType.PORTRAIT));
+            // 나랏글(가로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_DANMOUM, KeyType.PORTRAIT)){
+                TestCase_11_KOR_DANMOUM_PORT TC11 = new TestCase_11_KOR_DANMOUM_PORT(device, "DANMOUM_PORTRAIT");
+                runTime += "================= DANMOUM_PORTRAIT =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC11.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
+
+        try{
+            Log.i("@@@", "DANMOUM_LANDSCAPE - " + testPlan.getTestPlan(KeyType.KOR_DANMOUM, KeyType.LANDSCAPE));
+            // 단모음(가로모드) 입력
+            if(testPlan.getTestPlan(KeyType.KOR_DANMOUM, KeyType.LANDSCAPE)){
+                TestCase_12_KOR_DANMOUM_LAND TC12 = new TestCase_12_KOR_DANMOUM_LAND(device, "DANMOUM_LANDSCAPE");
+                runTime += "================= DANMOUM_LANDSCAPE =================\n";
+                runTime += device.RunTimeCheck("START");
+                TC12.start();
+                runTime += device.RunTimeCheck("END");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            runTime += "***Exception occurred***\n" + e.getMessage() + "\n";
+            runTime += device.RunTimeCheck("END");
+        }
     }
 
     private void initTest() throws IOException, UiObjectNotFoundException {
---
+++ app/src/androidTest/java/com/phillit/qa/basicinputtest/TestCase/TestCase_05_KOR_CHUNJIIN_PORT.java
@@ -0,0 +1,126 @@
+package com.phillit.qa.basicinputtest.TestCase;
+
+import android.os.RemoteException;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.util.Log;
+
+import com.phillit.qa.basicinputtest.Common.Configuration.Configuration;
+import com.phillit.qa.basicinputtest.Common.KeyType.KOR_ENG.Chunjiin;
+import com.phillit.qa.basicinputtest.Common.TestCaseParser;
+import com.phillit.qa.basicinputtest.Common.KeyType.KeyType;
+import com.phillit.qa.basicinputtest.Common.Device;
+import java.io.IOException;
+
+/**
+ * 테스트 명   : TestCase_05_KOR_CHUNJIIN_PORT
+ * 테스트 목적 : 세로모드상태에서 천지인의 입력을 검증한다.
+ * 테스트 순서 :
+ 1. Monkey Input 실행
+ 2. 세로모드
+ 3. /sdcard/QA/InputTest/TestWord.xls의 한글단어를 순차적으로 입력
+ */
+
+public class TestCase_05_KOR_CHUNJIIN_PORT {
+    String testType = "";
+    //String runTime;
+    StringBuffer word;
+    Device device;
+    KeyType Chunjiin;
+    TestCaseParser parser;
+    boolean isInternalTest = false;
+    int saveCnt = Configuration.RESULT_SAVE_COUNT;
+
+    public TestCase_05_KOR_CHUNJIIN_PORT(Device device, String testType) {
+        this.device = device;
+        this.testType = testType;
+        //this.runTime = "=================" + testType + "=================\n";
+        isInternalTest = device.getTestPlan().isInternalTest;
+        if(isInternalTest){
+            saveCnt = 10;
+        }
+    }
+
+    public void start() throws IOException, RemoteException, UiObjectNotFoundException {
+        Log.i("@@@", getClass().getName() + " start.");
+        ReadyTest();
+        Test();
+        FinishTest();
+    }
+
+    private void ReadyTest() throws RemoteException, UiObjectNotFoundException {
+        //runTime += device.RunTimeCheck("START");
+        // 천지인 키타입으로 변경
+        device.changeKeyType(KeyType.KOR_CHUNJIIN);
+
+        // Parser, KeyType init
+        parser = new TestCaseParser("kor", device.getContext());
+
+        Chunjiin = new Chunjiin(device, device.getContext(), KeyType.PORTRAIT, KeyType.KOR_CHUNJIIN);
+
+        // Monkey Input 실행
+        device.launchApplication("Monkey Input");
+
+        // 파일이름(테스트 타입) 입력
+        UiObject object = device.getUiDevice().findObject(new UiSelector().resourceId("com.phillit.qa.monkeyinput:id/edt_filename"));
+        if(object.waitForExists(5000)){
+            device.userWait(3000);
+            object.setText(testType);
+            device.userWait(3000);
+        }
+
+        // 입력필드 터치
+        device.touchObject("com.phillit.qa.monkeyinput:id/edt_input");
+        device.userWait(3000);
+
+        // 언어변경(한글)
+        device.changeKeyboardLanguage(KeyType.KOR_CHUNJIIN);
+
+        // 세로모드
+        // 10초 대기
+        device.getUiDevice().setOrientationNatural();
+        device.userWait(10000);
+    }
+
+    private void Test() throws IOException, RemoteException {
+        int i=1;
+        while(true){
+            word = parser.getWord(i);
+            if(word == null){
+                break;
+            }else{
+                device.inputMethod(word, Chunjiin);
+            }
+            if(i % saveCnt == 0){
+                device.touchObject("com.phillit.qa.monkeyinput:id/btn_save");
+                device.dumpsysMemifo(testType + "_meminfo");
+                device.userWait(5000);
+                if(device.getBatteryStatus() <= Configuration.BATTERY_MIN_VALUE){
+                    device.chargeDevice();
+                }
+            }
+            if(isInternalTest && i == Configuration.INTERNAL_TEST_COUNT){
+                break;
+            }
+            Log.i("@@@", "WordCnt : " + i + " / Word : " + word);
+            i++;
+        }
+        // 10초 대기
+        device.userWait(10000);
+    }
+
+    private void FinishTest() throws IOException {
+        // 언어변경(영어)
+        device.changeKeyboardLanguage(KeyType.KOR_CHUNJIIN);
+
+        // 다음 테스트시 불필요한 객체 해제
+        device.Release();
+        // 홈화면 이동
+        device.goToIdle();
+        // 10초 대기
+        device.userWait(10000);
+
+        //runTime += device.RunTimeCheck("END");
+    }
+}
